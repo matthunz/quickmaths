91aac6fc79c42a87b52c84d6566743dd033ee52c
Matt Hunzinger <matthunz2@gmail.com>
2022-07-19 14:13:11 -0400

    Fix and doc gcd function

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub mod stats;
 
 pub mod poly;
 
-use num::{traits::real::Real, FromPrimitive, Integer, One, Zero};
+use num::{traits::real::Real, FromPrimitive, Integer, One};
 
 pub fn ldexp(x: u32, exp: u32) -> u32 {
     x * 2u32.pow(exp)
@@ -24,6 +24,12 @@ pub fn epsilon<T: One + FromPrimitive + Real>() -> T {
     T::one() * T::from_u8(2).unwrap().powi(1 - (0f64.digits() as i32))
 }
 
+/// ```
+/// use quickmaths::gcd;
+/// 
+/// assert_eq!(gcd([1, 2]), 1);
+/// assert_eq!(gcd([3, 6]), 3);
+/// ```
 pub fn gcd<I>(integers: I) -> I::Item
 where
     I: IntoIterator,
@@ -32,5 +38,5 @@ where
     let mut iter = integers.into_iter();
     iter.next()
         .map(|i| iter.fold(i, |acc, i| acc.gcd(&i)))
-        .unwrap_or_else(|| I::Item::zero())
+        .unwrap_or_else(|| I::Item::one())
 }
