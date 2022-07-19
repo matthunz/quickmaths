a08eb1314822f211ad133fe7a7e415095ae0103b
Matt Hunzinger <matthunz2@gmail.com>
2022-07-19 18:44:21 -0400

    Create HarmonicSeries iterator

--- src/series.rs
+++ src/series.rs
@@ -1,7 +1,7 @@
 //! Series
 
-use crate::{ldexp, Digits};
-use num::{traits::real::Real, FromPrimitive, Zero};
+use crate::{fraction::Ratio, ldexp, Digits};
+use num::{traits::real::Real, FromPrimitive, Unsigned, Zero};
 
 /// ```
 /// use quickmaths::series::kahan_sum;
@@ -36,4 +36,47 @@ where
     }
 
     result
+}
+
+/// Iterator for the [harmonic series](https://en.wikipedia.org/wiki/Harmonic_series_(mathematics)).
+/// `Iterator::nth` skips directly to the `n`th term.
+pub struct HarmonicSeries<T> {
+    pub i: T,
+}
+
+impl<T> HarmonicSeries<T>
+where
+    T: Unsigned + FromPrimitive + Clone,
+{
+    // Create a new harmonic series starting at `i`.
+    pub fn new(i: T) -> Self {
+        Self { i }
+    }
+}
+
+impl<T> Default for HarmonicSeries<T>
+where
+    T: Unsigned + FromPrimitive + Clone,
+{
+    fn default() -> Self {
+        Self::new(T::one())
+    }
+}
+
+impl<T> Iterator for HarmonicSeries<T>
+where
+    T: Unsigned + FromPrimitive + Clone,
+{
+    type Item = Ratio<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let frac = Ratio::new(T::one(), self.i.clone());
+        self.i = self.i.clone() + T::one();
+        Some(frac)
+    }
+
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        self.i = T::from_usize(n - 1).unwrap();
+        self.next()
+    }
 }
